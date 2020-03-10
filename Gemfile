674d451a0fd9460fa40380c907f143c675e4c545
kento <kentz.eva.a.r.xxx@gmail.com>
2020-03-10 18:55:26 +0900

    いいね機能、コメント機能の実装

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.6.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~>1.3.6', group: :development 
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -40,6 +40,7 @@ gem 'jquery-rails'
 gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem 'mini_magick'
+gem 'pg', group: :production
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: { registrations: 'registrations' } 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'pages#home'
+  root  'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(new create) do
+  resources :posts, only: %i(new create index show destroy) do #ルーティングをネストすることで親子関係がわかる
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
   end
+  
 
 end
