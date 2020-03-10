43e345b784ecab6cd5ca56a0cf7308d3d091e197
kento <kentz.eva.a.r.xxx@gmail.com>
2020-03-10 19:17:42 +0900

    annotateの導入

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  image      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer          not null
+#
+# Indexes
+#
+#  index_photos_on_post_id  (post_id)
+#
 class Photo < ApplicationRecord
     belongs_to :post
 
--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  image      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer          not null
+#
+# Indexes
+#
+#  index_photos_on_post_id  (post_id)
+#
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
